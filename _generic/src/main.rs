c2129f65a9115aeca066397d27d9a491682364d0
OMAR SCICOLONE s296492 <omar.scicolone@gmail.com>
2022-04-14 00:08:47 +0200

    first commit

---
+++ _generic/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+
+    let v: i32 = 123;
+    
+    
+    let mut w = v;
+    w = -5;
+
+
+    let x = 1.3278;         //floating point a 64bit
+    let y = 1.3278f32;      //floating point a 32bit
+
+
+}
